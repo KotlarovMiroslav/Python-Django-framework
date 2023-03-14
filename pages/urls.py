fa735ecba9c101f8af67020ced08506cb91fc94f
KotlarovMiroslav <kotlarovmiroslav@gmail.com>
2023-03-14 15:55:48 +0200

    Added the post page!

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, PagesPageView
 
 urlpatterns = [
+    path("pages/", PagesPageView.as_view(), name='pages'),
     path("about/", AboutPageView.as_view(), name="about"),
     path('',HomePageView.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-
 # Create your views here.
 # from django.http import HttpResponse
 
 # def homePageView(request):
 #     return HttpResponse('Hello, World!')
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Post
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -14,3 +14,6 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
     template_name = "about.html"
 
+class PagesPageView(ListView):
+    model = Post
+    template_name = "pages.html"
