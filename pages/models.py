a2d18b74df9d0fd483eb8f8f7a3f3311a5553369
KotlarovMiroslav <kotlarovmiroslav@gmail.com>
2023-03-28 18:01:37 +0300

    update tests

--- pages/models.py
+++ pages/models.py
@@ -5,4 +5,4 @@ class Post(models.Model):
     text = models.TextField()
 
     def __str__(this):
-        return this.text[:50] + " | click here"
+        return this.text[:50] + " |click to read more|"
--- pages/tests.py
+++ pages/tests.py
@@ -1,41 +1,65 @@
-from django.test import SimpleTestCase
+
+from django.test import TestCase
 from django.urls import reverse
+
+from .models import Post
 
 # Create your tests here.
 
-class HomepageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(this):
-        response = this.client.get("/")
-        this.assertEqual(response.status_code, 200)
+# class PostTests(TestCase):
+#     @classmethod
+#     def setUpTestData(cls):
+#         cls.post = Post.objects.create(text="This is a test!")
+#     def test_model_content(self):
+#         self.assertEqual(self.post.text, "This is a test!")
+#     def test_url_exists_at_correct_location(self): # new
+#         response = self.client.get("/")
+#         self.assertEqual(response.status_code, 200)
+#     def test_url_available_by_name(self): # new
+#         response = self.client.get(reverse("home"))
+#         self.assertEqual(response.status_code, 200)
+#     def test_template_name_correct(self): # new
+#         response = self.client.get(reverse("home"))
+#         self.assertTemplateUsed(response, "home.html")
+#     def test_template_content(self): # new
+#         response = self.client.get(reverse("home"))
+#         self.assertContains(response, "This is a test!")
+
+
+
+# class HomepageTests(SimpleTestCase):
+#     def test_url_exists_at_correct_location(this):
+#         response = this.client.get("/")
+#         this.assertEqual(response.status_code, 200)
         
-    def test_url_available_by_name(this):
-        response = this.client.get(reverse("home"))
-        this.assertEqual(response.status_code, 200)
+#     def test_url_available_by_name(this):
+#         response = this.client.get(reverse("home"))
+#         this.assertEqual(response.status_code, 200)
 
-    def test_template_name_correct(this):
-        response = this.client.get(reverse("home"))
-        this.assertTemplateUsed(response, "home.html")
+#     def test_template_name_correct(this):
+#         response = this.client.get(reverse("home"))
+#         this.assertTemplateUsed(response, "home.html")
 
-    def test_template_content(this):
-        response = this.client.get(reverse("home"))
-        this.assertContains(response, "<h1>Homepage</h1>")
+#     def test_template_content(this):
+#         response = this.client.get(reverse("home"))
+#         this.assertContains(response, "<h1>Homepage</h1>")
 
         
             
-class AboutpageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(this):
-        response = this.client.get("/about/")
-        this.assertEqual(response.status_code, 200)
+# class AboutpageTests(SimpleTestCase):
+#     def test_url_exists_at_correct_location(this):
+#         response = this.client.get("/about/")
+#         this.assertEqual(response.status_code, 200)
 
-    def test_url_available_by_name(this):
-        response = this.client.get(reverse("about"))
-        this.assertEqual(response.status_code, 200)
+#     def test_url_available_by_name(this):
+#         response = this.client.get(reverse("about"))
+#         this.assertEqual(response.status_code, 200)
     
-    def test_template_name_correct(this):
-        response = this.client.get(reverse("about"))
-        this.assertTemplateUsed(response, "about.html")
+#     def test_template_name_correct(this):
+#         response = this.client.get(reverse("about"))
+#         this.assertTemplateUsed(response, "about.html")
 
-    def test_template_content(this):
-        response = this.client.get(reverse("about"))
-        this.assertContains(response, "<h1>About</h1>")
+#     def test_template_content(this):
+#         response = this.client.get(reverse("about"))
+#         this.assertContains(response, "<h1>About</h1>")
         
